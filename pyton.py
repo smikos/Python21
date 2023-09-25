c444fef791370ea71b09d70211ee060de5d050cb
Konstantin Smirnov <sasharugby@yandex.ru>
2023-09-25 21:29:06 +0300

    goodDz1

--- pyton.py
+++ pyton.py
@@ -1,1 +1,17 @@
-print("f")
+n = int(input("Количество монет:"))
+orel=0
+resh=0
+
+for i in range(n):
+    x=int(input("Сторона монеты(1 или 0): "))
+    if x ==1: 
+      orel+=1
+    else :
+      resh +=1
+if orel > resh :
+   print(resh)
+   
+else :
+    print (orel)
+
+    
